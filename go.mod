52b2008899be4826a5f7a94abccab92587d53ea3
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-04-26 14:56:23 +0200

    Testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Skarlso/test
 
 go 1.18
+
+require github.com/davecgh/go-spew v1.1.1
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+type S struct {
+	Name string
+}
 
 func main() {
 	fmt.Println("vim-go")
+	spew.Dump(&S{Name: "bla"})
 }
